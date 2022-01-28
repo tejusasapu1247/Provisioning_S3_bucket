066078acf675bf365bf1c9377a920216eee0827c
tejusasapu1247 <67572528+tejusasapu1247@users.noreply.github.com>
2022-01-28 11:20:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ tags = {
 }
 
 resource "aws_s3_bucket" "demos3" {
-    bucket = "${var.bucket_name}" 
-    acl = "${var.acl_value}"   
+    bucket = "bucket_from_terraform_cloud" 
+    acl = "private"   
 }
 
 
